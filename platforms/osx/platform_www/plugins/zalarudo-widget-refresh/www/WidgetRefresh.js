fd98d5eeeb58e4dd12a2d07a70c102bbb16eb810
misikovbrite <hello@britetodo.com>
2023-05-03 22:38:56 +0200

    remove platform from git track

--- platforms/osx/platform_www/plugins/zalarudo-widget-refresh/www/WidgetRefresh.js
+++
@@ -1,8 +0,0 @@
-cordova.define("zalarudo-widget-refresh.WidgetRefresh", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.refresh = function (arg0, success, error) {
-    exec( success, error, 'WidgetRefresh', 'refresh', [arg0] );
-};
-
-});

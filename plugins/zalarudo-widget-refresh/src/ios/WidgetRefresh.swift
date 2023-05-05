aeefef4d7c725f6345a96c8ba7a3d6dada75e971
misikovbrite <hello@britetodo.com>
2023-05-05 09:50:58 +0200

    Fixed capacitor

--- plugins/zalarudo-widget-refresh/src/ios/WidgetRefresh.swift
+++ plugins/zalarudo-widget-refresh/src/ios/WidgetRefresh.swift
@@ -1,19 +1,26 @@
+import Cordova
 import WidgetKit
 
 @objc(WidgetRefresh) class WidgetRefresh : CDVPlugin {
     @objc (refresh:)
     func refresh (command: CDVInvokedUrlCommand){
-        
+
         var pluginResult = CDVPluginResult(
             status: CDVCommandStatus_ERROR
           )
-        
 
-            
-        WidgetCenter.shared.reloadTimelines(ofKind: widgetId)
-            
+        let widgetId = command.arguments[0] as? String ?? ""
+
+        if widgetId.count > 0 {
+
+            if #available(iOS 14.0, *) {
+                WidgetCenter.shared.reloadTimelines(ofKind: widgetId)
+            } else {
+                // Fallback on earlier versions
+            }
 
 
+        }
         
         self.commandDelegate!.send(
               pluginResult,
